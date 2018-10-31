4af3127abde33df2500b62f0b24d10501b9262b5
scanopm <Stephane.Cano@pmi.com>
2018-10-31 11:50:40 +0100

    Check the configuration file is readable

--- rpackutils/config.py
+++ rpackutils/config.py
@@ -18,12 +18,18 @@ class Config:
     Holds configuration file properties
     """
     def __init__(self, filepath):
-        if not os.path.exists(filepath):
+        if not os.path.isfile(filepath):
             logger.error('Configuration file \"{0}\" not found!'
                          .format(filepath))
             raise FileNotFoundError(errno.ENOENT,
                                     os.strerror(errno.ENOENT),
                                     filepath)
+        if not os.access(filepath, os.R_OK):
+            logger.error('Configuration file \"{0}\" is not readable(no access)!'
+                         .format(filepath))
+            raise PermissionError(errno.EACCESS,
+                                  os.strerror(errno.EACCESS),
+                                  filepath)
         self._config_file = filepath
         self._environment_config = configparser.ConfigParser()
         self._environment_config.read(self._config_file)
